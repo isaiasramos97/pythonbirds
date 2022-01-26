3194a57a96f14cadffa8b7d09a50cad8a226ca80
Isaias <isaiasramos_@outlook.com>
2022-01-26 18:53:59 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,8 @@
 class Pessoa:
-    def __init__(self, *filhos, nome=None, idade=35):
+    olhos = 2   # chamado de atributo default ou atributo de classe
+    # este atributo nao entra no __dict__
+
+    def __init__(self, *filhos, nome=None, idade=35):    # atributos de instância
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
@@ -20,5 +23,9 @@ if __name__ == '__main__':
         print(filho.nome)
     luciano.sobrenome = 'Ramalho'
     del luciano.filhos
+    luciano.olhos = 1   # neste caso, entra no __dict__
     print(luciano.__dict__)
     print(renzo.__dict__)
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos))
